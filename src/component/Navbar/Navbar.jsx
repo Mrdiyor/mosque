4eab28be68186e4403d3b330eb922f86a0ba4162
Mrdiyor <144511285+Mrdiyor@users.noreply.github.com>
2023-09-20 10:50:37 +0500

    navbar

---
+++ src/component/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+// import React from 'react'
+import "./Navbar.scss";
+
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <a href="/" className="logo">
+          <img src="../../../public/assets/logo.png.crdownload" alt="png" />
+        </a>
+        <div className="menu">
+          <li><a href="/">Home</a></li>
+          <li><a href="/">Beautiful Mosques</a></li>
+          <li><a href="/">Locations</a></li>
+          <li><a href="/">Imams</a></li>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
